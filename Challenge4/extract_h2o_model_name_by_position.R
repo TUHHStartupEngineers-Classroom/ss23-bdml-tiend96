9d4cf60ce068c48482caeb7f90ae08937bf8848f
tiend96 <tien.danny@gmail.com>
2023-05-24 21:10:53 +0200

    Challenge 4 Finished
    Finished Challenge 4

---
+++ Challenge4/extract_h2o_model_name_by_position.R
@@ -0,0 +1,12 @@
+extract_h2o_model_name_by_position <- function(h2o_leaderboard, n = 1, verbose = T) {
+  
+  model_name <- h2o_leaderboard %>%
+    as.tibble() %>%
+    slice(n) %>%
+    pull(model_id)
+  
+  if (verbose) message(model_name)
+  
+  return(model_name)
+  
+}
